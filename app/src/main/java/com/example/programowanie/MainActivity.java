69985b54a899c6b666596c9904bfa1f693880ebd
Marcin Hipnarowicz <mar.hipnarowicz@gmail.com>
2020-01-28 17:52:56 +0100

    aaaa

--- app/src/main/java/com/example/programowanie/MainActivity.java
+++ app/src/main/java/com/example/programowanie/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.programowanie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addText;
+    private Button sortText;
+    private EditText addFirstNameET;
+    private EditText addNameET;
+    private ListView listView;
+    ArrayList<String> listName = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        addText = (Button) findViewById(R.id.buttonAdd);
+        addFirstNameET = (EditText) findViewById(R.id.editTextFirstName);
+        addNameET = (EditText) findViewById(R.id.editTextName);
+        listView = (ListView) findViewById(R.id.listview);
+        sortText = (Button) findViewById(R.id.buttonSort);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, listName);
+        listView.setAdapter(arrayAdapter);
+
+        addText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(addText.getText() != null)
+                {
+                    String name;
+
+                    name = addFirstNameET.getText().toString() + " " + addNameET.getText().toString();
+
+                    listName.add(name);
+
+
+                }
+            }
+        });
+        sortText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
